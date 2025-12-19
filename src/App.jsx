98d2de6acd0fe7e68401740a5d41f93b9b811082
Mahek Sheth <maheksheth77@gmail.com>
2025-12-19 20:01:06 +0530

    Day1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./Header";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App(){
   return (
-    <>
-      <h1>Hello React JS - DAY 1</h1>
-      <button>Hello</button>
-    </>
+    <div>
+      <h1>Welcome to the App</h1>
+      <Header />
+    </div>
   )
 }
-
-export default App
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,6 @@
+function Header(){
+    return <div>
+        <h2>Header data</h2>
+    </div>
+}
+export default Header;
