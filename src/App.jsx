b90677eae8d5b01f829684667925159af9a75a30
Mahek Sheth <maheksheth77@gmail.com>
2025-12-20 19:43:20 +0530

    JSX

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import Header from "./Header";
 import Login, { Signup, User } from "./UserComponent";
 
 function App(){
+  function handleClick(){
+    alert("Button Clicked");
+  }
   return (
     <div>
       <h1>React JS - Day 2</h1>
+      <button onClick={handleClick}>Click Me</button>
       <Header />
       <Login />
       <Signup />
