82a08441d0ae218bad054ef88e04b9ca19ecc4e7
Mahek Sheth <maheksheth77@gmail.com>
2025-12-23 22:00:57 +0530

    Counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,6 +1,7 @@
 import{useState} from "react";
 const Counter=()=>{
     const [count,setCount]=useState(0);
+    const [rCount, setRCount]=useState(10);
     function increment(){
         setCount(count+1);
     }
@@ -9,6 +10,8 @@ const Counter=()=>{
             <h1>Counter Component</h1>
             <button onClick={increment}>Count ++</button>
             <h2>Count Value: {count}</h2>
+            <button onClick={()=>{setRCount(rCount-1)}}>Count--</button>
+            <h2>RCount Value: {rCount}</h2>
         </div>
     )
 }
