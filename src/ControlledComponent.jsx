0fc89b6cb2b105df31d231f652c5579d5858091e
Mahek Sheth <maheksheth77@gmail.com>
2026-01-02 19:49:11 +0530

    props

--- src/ControlledComponent.jsx
+++ src/ControlledComponent.jsx
@@ -6,7 +6,7 @@ const ControlledComponent = () => {
     const [password,setPassword]=useState("");
     return (
         <div>
-            <form action="" method='GET'>
+            <form action="" method='GET' style={{color:"orange",border:"2px solid blue",margin:"10px",padding:"10px",width:"200px"}}>
                 <input type="text" value={name} onChange={(event)=>{
                     setName(event.target.value)
                 }} placeholder='Enter name' /><br />
