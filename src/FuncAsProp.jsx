9767546513597ecca0f40fd0cb9694e217eaabae
Mahek Sheth <maheksheth77@gmail.com>
2026-01-08 11:10:51 +0530

    Passing function as props

---
+++ src/FuncAsProp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FuncAsProp = () => {
+  return (
+    <div>
+      <h1>Passing function as props in component</h1>
+    </div>
+  )
+}
+
+export default FuncAsProp
