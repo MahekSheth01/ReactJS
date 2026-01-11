3d8a5f71ba72c76121661f1924b84589a5a5ba94
Mahek Sheth <maheksheth77@gmail.com>
2026-01-11 18:27:51 +0530

    useId Hook

--- src/HookuseId.jsx
+++ src/HookuseId.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useId } from 'react'
 
 const HookuseId = () => {
+  const name=useId();
+  const password=useId();
+  const terms=useId();
+
   return (
     <div>
       <h1>UseId hook</h1>
-      
+      <h3>{name}</h3>
+      <h3>{password}</h3>
+      <h3>{terms}</h3>
     </div>
   )
 }
