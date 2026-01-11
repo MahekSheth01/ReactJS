2de2de78e2236c8067e943a61ee4a9d0d8947296
Mahek Sheth <maheksheth77@gmail.com>
2026-01-11 18:35:42 +0530

    useId Hook

--- src/HookuseId.jsx
+++ src/HookuseId.jsx
@@ -1,18 +1,32 @@
 import React, { useId } from 'react'
 
 const HookuseId = () => {
-  const name=useId();
-  const password=useId();
-  const terms=useId();
 
   return (
     <div>
-      <h1>UseId hook</h1>
-      <h3>{name}</h3>
-      <h3>{password}</h3>
-      <h3>{terms}</h3>
+    <UseForm/>
     </div>
   )
 }
 
 export default HookuseId
+
+function UseForm(){
+    const name=useId();
+  const password=useId();
+  const terms=useId();
+  return(
+    <div>
+      <h1>UseId hook</h1>
+      <h3>{name}</h3>
+      <h3>{password}</h3>
+      <h3>{terms}</h3>
+      <form action="" method='POST'>
+        <label htmlFor={name}>Enter Name : </label> 
+        <input type="text" id="name" placeholder='Enter Name' name=""  /><br /><br />
+         <label htmlFor={password}>Enter Password : </label> 
+        <input type="password" id="password" placeholder='Enter password' name=""  />
+      </form>
+    </div>
+  )
+}
